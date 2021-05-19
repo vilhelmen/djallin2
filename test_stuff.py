e7fc6bb46ae97ab65bc426dd3535e57e99152356
Will Starms <vilhelmen@gmail.com>
2021-05-18 19:00:23 -0500

    token receiver work

--- test_stuff.py
+++ test_stuff.py
@@ -3,10 +3,11 @@
 import socket
 import ssl
 import requests
-
+import webbrowser
 import logging
 import argparse
 import toml
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
 
 sock_context = ssl.create_default_context()
@@ -24,41 +25,62 @@ class ProtectedString(str):
         return str.__str__(self)
 
 
+class TokenReceiver(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self._token_data = None
+        super().__init__(*args, **kwargs)
+
+    def serve_forever(self):
+        while not self._token_data:
+            self.handle_one_request()
+        return self._token_data
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        # TODO: test this is the http request/url/whatever
+        print(self.requestline)
+        self._set_headers()
+        self.wfile.write("""<html><body style="margin-left: 30%; margin-top: 10%; background: rgb(89, 174, 88); color: white;"><div><script>setTimeout("window.close()",3000)</script><p style="font-size: 256px;">;)</p></body></html>""".encode('utf-8'))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+
 def validate_token(oauth_token):
     """
     Check token status with twitch
-    :param oauth_token:
-    :return:
+    Raises on connection error or unexpected status code from twitch
+    :param oauth_token: user token
+    :return: validation response if valid or None
     """
+    logging.info('Calling Papa Bezos...')
     try:
         resp = requests.get('https://id.twitch.tv/oauth2/validate',
                             headers={'Authorization': 'OAuth ' + oauth_token})
     except Exception as err:
-        logging.critical('Error contacting twitch, internet issues? %s', err)
-        raise
-    if resp.status_code == 200:
-        logging.info('Twitch says our token is valid')
-        resp = resp.json()
-        # {'client_id': 'r50bzaj62mdvoo3nojyfuqeewxlj23', 'login': 'vilhel',
-        #  'scopes': ['channel:read:redemptions', 'chat:read'],
-        #  'user_id': '64773936', 'expires_in': 4902655}
-        # check scopes
-        missing_scopes = {'channel:read:redemptions', 'chat:read'} - set(resp['scopes'])
-        if missing_scopes:
-            # man %s feel so ancient and gross
-            logging.critical('Missing scopes from token (%s)! Discarding it.', missing_scopes)
-            return None
-        # Uhhhhhh seconds? Let's renew if we're under a week? Could probably do a day or two.
-        elif resp['expires_in'] < 60 * 24 * 7:
-            logging.info('Token is expiring soon, throwing it out.')
-            return None
-        # TODO return data blob
-        return {
+        msg = f'Error contacting Twitch, internet issues? {err}'
+        logging.critical(msg)
+        raise RuntimeError(msg) from err
 
-        }
-    if resp.status_code == 401:
-        logging.info('Existing token looks bad, will need to get a new one')
+    if resp.status_code == 200:
+        resp = resp.json()
+        need = {'channel:read:redemptions', 'chat:read'}
+        has = set(resp['scopes'])
+        missing = need - has
+        if missing:
+            logging.warning(f'Token is missing required scopes: {missing}. Throwing it out.')
+            return None
+        return resp
+    elif resp.status_code == 401:
         return None
+    else:
+        msg = f'Unrecognized status code during token check: {resp.status_code}, unsure how to proceed.'
+        logging.critical(msg)
+        raise RuntimeError(msg)
 
 
 def launch_config():
@@ -66,21 +88,41 @@ def launch_config():
     Load the config file, vaguely check its shape, dial in to twitch/generate token
     :return: config dict
     """
+    # TODO: rename config file, figure out .gitignore for it
+    config_file = Path('creds.toml')
     try:
-        # TODO: rename config file, figure out .gitignore for it
-        with open('creds.toml', 'r') as f:
-            config = toml.load(f)
+        config = toml.loads(config_file.read_text())
     except Exception as err:
         logging.critical('Error loading configuration file: %s', err)
         raise
 
     # TODO: validate config shape here
 
-    has_token = False
+    ready = False
     if config.get('token'):
-        has_token = True
+        # Hide our token, check if it's good
         config['token'] = ProtectedString(config['token'])
-        validate_token(config['token'])
+        validation_response = validate_token(config['token'])
+        if validation_response is None:
+            logging.warning('Existing token looks bad, will need to get a new one')
+        else:
+            if validation_response['expires'] < 60*24*5:
+                logging.info('Token is expiring soon, requesting a new one!')
+            else:
+                ready = True
+
+        if not ready:
+            # BE GONE FROM ME SHITTY TOKEN
+            del config['token']
+            config_file.write_text(toml.dumps(config))
+
+    if not ready:
+        logging.info('Booting HTTP server and requesting a new token')
+        # Print URL as well!!
+        try:
+            httpd = HTTPServer(('localhost', 42069), TokenReceiver)
+
+        webbrowser.open('', new=2, autoraise=True)
 
     # There's not a great way to do this. We gotta ping twitch to check expiration/validity
     # But we may want to pull a new token, or have to if we don't have one
