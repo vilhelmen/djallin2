c07d35498f137862bc88dcc6d9132118e41f3b99
Will Starms <vilhelmen@gmail.com>
2021-05-21 23:06:11 -0500

    token getter seems good!

--- test_stuff.py
+++ test_stuff.py
@@ -2,10 +2,14 @@
 
 import socket
 import ssl
+import urllib.parse
+
 import requests
+import concurrent.futures
 import webbrowser
 import logging
 import argparse
+import secrets
 import toml
 import threading
 from urllib.parse import parse_qs
@@ -19,7 +23,7 @@ logging.basicConfig(level=logging.DEBUG)
 receiver_html = """
 <html><body style="margin-left: 30%; margin-top: 10%; background: rgb(233, 233, 45); color: white;"><div>
     <p style="font-size: 256px;">:|</p>
-    <form id="token_form" hidden method="POST" action="/">
+    <form id="token_form" hidden method="POST" action="/" enctype="application/x-www-form-urlencoded">
         <input type="text" id="token_string" name="token_string">
     </form>
     <script>
@@ -34,40 +38,43 @@ receiver_html = """
 accepted_html = """
 <html><body style="margin-left: 30%; margin-top: 10%; background: rgb(89, 174, 88); color: white;"><div>
     <p style="font-size: 256px;">;)</p>
-    <script>setTimeout("window.close()",3000)</script>
+    <script>setTimeout("window.close()",5000)</script>
 </div></body></html>
 """.encode('utf-8')
 
 rejected_html = """
 <html><body style="margin-left: 30%; margin-top: 10%; background: rgb(217, 33, 4); color: white;"><div>
     <p style="font-size: 256px;">:(</p>
-    <script>setTimeout("window.close()",3000)</script>
+    <script>setTimeout("window.close()",5000)</script>
 </div></body></html>
 """.encode('utf-8')
 
 
-class ProtectedString(str):
-    def __str__(self):
-        return '---NO---'
+# class ProtectedString(str):
+#     def __str__(self):
+#         return '---NO---'
+#
+#     __repr__ = __str__
+#
+#     def _orig_str(self):
+#         return str.__str__(self)
 
-    __repr__ = __str__
 
-    def _orig_str(self):
-        return str.__str__(self)
+http_responded = threading.Event()
+http_state = secrets.token_urlsafe(16)
+http_response = tuple()  # >:C globals
+authorize_url = 'https://id.twitch.tv/oauth2/authorize?' + urllib.parse.urlencode(
+    {'response_type': 'token',
+     'client_id': 'r50bzaj62mdvoo3nojyfuqeewxlj23',
+     'redirect_uri': 'http://localhost:42069',
+     'scope': 'channel:read:redemptions chat:read',
+     'state': http_state}  # set force_verify to true to prompt authorization every time
+)
 
 
+# Looks like this is instantiated per incoming request >:C
+# There's really not a good way to get data out of this thing.
 class TokenReceiver(BaseHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        self._token_data = None
-        self._expected_state = None
-        super().__init__(*args, **kwargs)
-
-    def serve_forever(self, expected_state):
-        self._expected_state = expected_state
-        while not self._token_data:
-            self.handle_one_request()
-        return self._token_data
-
     def _set_headers(self, code=200):
         self.send_response(code)
         self.send_header("Content-type", "text/html")
@@ -81,29 +88,36 @@ class TokenReceiver(BaseHTTPRequestHandler):
         self._set_headers()
 
     def do_POST(self):
+        global http_response
         # Hoo and do I mean TRY
         try:
             logging.info('Received response from browser')
             form_resp = self.rfile.read(int(self.headers['Content-Length'], 0)).decode('utf-8')
-            parsed_token = parse_qs(parse_qs(form_resp)['token_string'][0])['access_token']
+            parsed_resp = parse_qs(parse_qs(form_resp)['token_string'][0])
+            if parsed_resp['state'][0] != http_state:
+                raise RuntimeError('Security token mismatch, something is up with our Twitch connection')
+            parsed_token = parsed_resp['access_token'][0]
         except Exception as err:
-            self._token_data = err
+            http_response = err
         else:
             try:
                 logging.info('Validating new token')
                 validation_data = validate_token(parsed_token)
                 if validation_data is None:
-                    raise RuntimeError(f'Error running new token validation: {err}')
+                    raise RuntimeError(f'New token validation failed :(')
             except Exception as err:
-                self._token_data = err
+                http_response = err
             else:
-                self._token_data = validation_data
-        if isinstance(self._token_data, Exception):
+                http_response = (parsed_token, validation_data)
+
+        if isinstance(http_response, Exception):
             resp = rejected_html
         else:
             resp = accepted_html
+
         self._set_headers()
         self.wfile.write(resp)
+        http_responded.set()
 
 
 def validate_token(oauth_token):
@@ -113,7 +127,7 @@ def validate_token(oauth_token):
     :param oauth_token: user token
     :return: validation response if valid or None
     """
-    logging.info('Calling Papa Bezos...')
+    logging.info('Phoning Papa Bezos...')
     try:
         resp = requests.get('https://id.twitch.tv/oauth2/validate',
                             headers={'Authorization': 'OAuth ' + oauth_token})
@@ -139,6 +153,54 @@ def validate_token(oauth_token):
         raise RuntimeError(msg)
 
 
+def token_registration():
+    """
+    Boot an HTTP listener and contact twitch for a new token.
+    Raises on failure
+    :return: (token, validation_data)
+    """
+    logging.info('Booting HTTP server')
+    try:
+        server = HTTPServer(('localhost', 42069), TokenReceiver)
+        http_thread = threading.Thread(target=server.serve_forever, name='http_worker')
+        http_thread.start()
+    except Exception as err:
+        logging.critical(f'Error launching HTTP listener: {err}')
+        server = None
+        http_thread = None
+
+    logging.info(f'Opening browser to {authorize_url}')
+    browser_opened = webbrowser.open(authorize_url, new=2, autoraise=True)
+    if not browser_opened:
+        logging.error('Browser open failed :(')
+
+    if server is None:
+        logging.critical('HTTP listener is down, cannot receive token.\n'
+                         'Refer to the documentation for manual authorization.\n'
+                         'Closing in 10...')
+        # time.sleep is bad
+        # FIXME: see if the windows bundler can leave the window open on exit
+        shutoff = threading.Event()
+        shutoff.wait(10)
+        raise RuntimeError('Failure in HTTP listener')
+
+    logging.info('Waiting for response...')
+    http_responded.wait()
+    logging.info('Listener cleanup')
+    server.shutdown()
+    http_thread.join()
+
+    if isinstance(http_response, Exception):
+        logging.critical(f'HTTP responder returned an error: {http_response}.\n'
+                         'Refer to the documentation for manual authorization.\n'
+                         'Closing in 10...')
+        # time.sleep is bad
+        shutoff = threading.Event()
+        shutoff.wait(10)
+        raise RuntimeError('Failure in HTTP listener') from http_response
+    return http_response
+
+
 def launch_config():
     """
     Load the config file, vaguely check its shape, dial in to twitch/generate token
@@ -152,68 +214,94 @@ def launch_config():
         logging.critical('Error loading configuration file: %s', err)
         raise
 
-    # TODO: validate config shape here
+    # TODO: validate config shape here?
 
     ready = False
     if config.get('token'):
         # Hide our token, check if it's good
-        config['token'] = ProtectedString(config['token'])
         validation_response = validate_token(config['token'])
         if validation_response is None:
             logging.warning('Existing token looks bad, will need to get a new one')
         else:
-            if validation_response['expires'] < 60*24*5:
-                logging.info('Token is expiring soon, requesting a new one!')
+            if validation_response['expires_in'] < 60*24*5:
+                logging.info('Token is expiring soon, requesting a new one')
             else:
                 ready = True
 
         if not ready:
             # BE GONE FROM ME SHITTY TOKEN
-            del config['token']
+            config['token'] = ''
+            logging.info('Deleting old token')
             config_file.write_text(toml.dumps(config))
 
     if not ready:
-        logging.info('Booting HTTP server and requesting a new token')
-        # Print URL as well!!
-        try:
-            # TODO: test/finish/etc
-            server = HTTPServer(('localhost', 42069), TokenReceiver)
-            http_thread = threading.Thread(target=server.serve_forever)
+        token_registration()
+        config['token'] = http_response[0]
+        logging.info('Saving new token')
+        config_file.write_text(toml.dumps(config))
+        validation_response = http_response[1]
 
-        except Exception as err:
-            pass
-        #webbrowser.open('', new=2, autoraise=True)
-    # There's not a great way to do this. We gotta ping twitch to check expiration/validity
-    # But we may want to pull a new token, or have to if we don't have one
-    # which we then need to ping twitch for.
-    # So we need to (maybe) ping twitch, (maybe) reauth, and then (maybe) ping twitch again
-    return config
-
-
+    return config, validation_response
 
 
 if __name__ == '__main__':
+    config, server_validation = launch_config()
 
-    config = launch_config()
+    #print(config, server_validation)
 
-    token_data = requests.get('https://id.twitch.tv/oauth2/validate', heades={'Authorization': 'OAuth {}'.format(creds['oauth'])})
+    creds = {
+        'host': 'irc.chat.twitch.tv',
+        'port': 6697,
+        'user': server_validation['login'],
+        'channel': '#' + server_validation['login'],
+        'oauth': 'oauth:' + config['token']
+    }
+
+    # TODO: ugh. we're gonna have to put everything in a try and eugh
+    retry_count = 0
 
     with socket.create_connection((creds['host'], creds['port'])) as sock:
         sock.setblocking(True)  # The docs DON'T say it DOESN'T work on windows
         # I... do you set the wrapper or...???????????
         # ssock.setblocking(True)
         with sock_context.wrap_socket(sock, server_hostname=creds['host']) as ssock:
-            ssock.send('PASS {}\r\nNICK {}\r\nJOIN {}\r\n'.format(
-                ''.join(['oauth:', creds['oauth']]), creds['user'], creds['channel']).encode('utf-8'))
+            ssock.send('PASS {}\r\nNICK {}\r\n'.format(creds['oauth'], creds['user']).encode('utf-8'))
             # ":tmi.twitch.tv NOTICE * :Login authentication failed" on bad oauth. idk about expired
             # We should expect a "GLHF!" otherwise in the first response
+            login_status = 'GLHF!' in ssock.recv(512).decode('utf-8').strip()
+            if 'GLHF!' not in login_status:
+                # FIXME: retry...?
+                #  A recv != a connection issue
+                msg = f'Failed to login to chat: {login_status}'
+                logging.critical(msg)
+                raise RuntimeError(msg)
+
+            ssock.send('CAP REQ :twitch.tv/tags\r\n'.encode('utf-8'))
+            cap_add_status = ssock.recv(512).decode('utf-8').strip()
+            if cap_add_status != ':tmi.twitch.tv CAP * ACK :twitch.tv/tags':
+                msg = f'Failed to activate tag cap: {cap_add_status}'
+                logging.critical(msg)
+                raise RuntimeError(msg)
+
+            ssock.send('JOIN {}\r\n'.format(creds['channel']).encode('utf-8'))
+            join_status = ssock.recv(512).decode('utf-8').strip()
+            if 'End of /NAMES list' not in join_status:
+                msg = f'Failed to join chat: {join_status}'
+                logging.critical(msg)
+                raise RuntimeError(msg)
+
+            logging.info('Connected to chat!')
+
             # Every 5 minutes we get "PING :tmi.twitch.tv" to which we reply "PONG :tmi.twitch.tv"
             while True:
                 msg = ssock.recv(1024).decode('utf-8').strip()
                 # max twitch message len is 512 MAYBE
                 # It's IRC in that it's mostly IRC-shaped
+                # Tags probably blast that incredibly
                 if len(msg):
                     print('NEWMSG:', msg)
+                    #if msg == 'PING :tmi.twitch.tv':
                 else:
+                    # FIXME: idk why this happens but the login checks should prevent this now
                     print('.', end='')
 
